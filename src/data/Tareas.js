61067fd5fb1fda7b47273e7844f618d384245f14
Rusty <sarahimartinez73593@gmail.com>
2024-05-08 18:35:38 -0600

    Archivos js de informacion de las tareas (acciones) verdes, y de informacion de usuarios

---
+++ src/data/Tareas.js
@@ -0,0 +1,60 @@
+export const Tareas = [
+    {
+        id: 1,
+        titulo: 'Recoger colillas de cigarro',
+        descripcion: 'Recoger colillas de cigarro y colocarlas en los lugares asignados para desecharlas',
+        recompensa: 10,
+
+    },
+    {
+        id: 2,
+        titulo: 'Recolectar basura',
+        descripcion: 'Recolecta 3 items de basura en tu escuela',
+        recompensa: 3,
+    },
+    {
+        id: 3,
+        titulo: 'Rellenar tu botella de agua en el filtro',
+        descripcion: 'Reusar una botella para tomar agua, rellenarla en el filtro del edificio de salones A',
+        recompensa: 5,
+    },
+    {
+        id: 4,
+        titulo: 'Recoger colillas de cigarro',
+        descripcion: 'Recoger colillas de cigarro y colocarlas en los lugares asignados para desecharlas',
+        recompensa: 10,
+    },
+    {
+        id: 5,
+        titulo: 'Usar contenedor en el Comal++',
+        descripcion: 'Llevar tu propio tupper al comal al pedir comida para llevar',
+        recompensa: 5,
+    },
+    {
+        id: 5,
+        titulo: 'Usar contenedor en el Comal++',
+        descripcion: 'Llevar tu propio tupper al comal al pedir comida para llevar',
+        recompensa: 5,
+    },
+    {
+        id: 6,
+        titulo: 'Usar transporte público',
+        descripcion: 'Usar transporte publico para llegar a la facultad',
+        recompensa: 0.5,
+    }
+
+];
+
+export const TareasUsuario = [
+    {
+        id: 1,
+        imagenes: '',
+        comentario: 'Había un par de colillas en el coliseo y las puse en el bote de basura'
+    },
+    {
+        id: 6,
+        imagenes: '',
+        comentario: 'Había un par de colillas en el coliseo y las puse en el bote de basura'
+    }
+
+]
---
+++ src/data/Usuarios.js
@@ -0,0 +1,38 @@
+export const Usuarios = [
+    {
+        id: 1,
+        nombre: 'Ruth Sarahi Martinez Sánchez',
+        usuario: 'Rusty',
+        contraseña: 'contraseñasegura',
+        fechaNac: '14/01/2003',
+        correo: 'sarahi@gmail.com',
+        INE: ''
+    },
+    {
+        id: 2,
+        nombre: 'Samuel Samuel Sam',
+        usuario: 'SamGolpeado',
+        contraseña: 'samsclub',
+        fechaNac: '12/05/2003',
+        correo: 'sams@gmail.com',
+        INE: ''
+    },
+    {
+        id: 3,
+        nombre: 'Ludwicka Aguirre',
+        usuario: 'Lud<3',
+        contraseña: 'contraseña123',
+        fechaNac: '01/06/2001',
+        correo: 'lud@gmail.com',
+        INE: ''
+    },
+    {
+        id: 4,
+        nombre: 'Daniela Wuotto',
+        usuario: 'DaniW',
+        contraseña: 'sushiii',
+        fechaNac: '00/00/0000',
+        correo: 'dani@gmail.com',
+        INE: ''
+    }
+]
