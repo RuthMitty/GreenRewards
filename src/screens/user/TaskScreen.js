35cce0abc552fa97aaef33744c41b8094b5354a6
Rusty <sarahimartinez73593@gmail.com>
2024-05-20 17:42:29 -0600

    no c kkk

--- src/screens/user/TaskScreen.js
+++ src/screens/user/TaskScreen.js
@@ -6,7 +6,8 @@ import ImageBg from '../user/Group20.png';
 import BottomBar from '../../components/BottomBar';
 import TaskButton from '../../components/TaskButton'; 
 import { Tareas } from '../../data/Tareas';
-import { SimpleLineIcons } from '@expo/vector-icons';
+import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
+
 
 const TaskScreen = () => {
   const [expandedTask, setExpandedTask] = useState(null);
