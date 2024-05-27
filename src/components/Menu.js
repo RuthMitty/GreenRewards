6f76525b3f193b528c4895a9b14d4d0377401c13
Wuotto-D <danielwuotto@gmail.com>
2024-05-26 19:58:25 -0600

    added some changes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import TaskScreen from "../screens/user/Profile";
-import Home from "../screens/user/Actions";
+import MyRewards from "../screens/user/MyRewards";
 
 const Drawer = createDrawerNavigator();
 
@@ -17,7 +17,7 @@ const Menu = () => {
         />
         <Drawer.Screen
           name="Mis recompensas"
-          component={Home}
+          component={MyRewards}
           options={{ headerShown: false }} 
         />
         
---
+++ src/screens/user/MyRewards.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View} from 'react-native';
+
+const MyRewards = () =>{
+
+
+
+return (
+    <View>
+        <Text></Text>
+    </View>
+)
+
+};
