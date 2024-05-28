34f8a6cceb40057b6cd5a589d9c53d16888b6a6e
Rusty <sarahimartinez73593@gmail.com>
2024-05-27 18:55:26 -0600

    se recuperó el archivo de recompensas

---
+++ src/data/Recompensas.js
@@ -0,0 +1,40 @@
+export const Recompensas = [
+    {
+        id: 1,
+        titulo: 'Recompensa de prueba',
+        descripcion: 'Esta el la decripcion de una recompensa de prueba',
+        puntosNecesarios: 100
+
+    },
+    {
+        id: 2,
+        titulo: 'Descuento en el comal++',
+        descripcion: 'Esta el la decripcion de una recompensa de prueba',
+        puntosNecesarios: 100
+    },
+    {
+        id: 3,
+        titulo: 'Curso de Educacion Continua',
+        descripcion: 'Esta el la decripcion de una recompensa de prueba',
+        puntosNecesarios: 100
+    },
+    {
+        id: 4,
+        titulo: 'Descuento en la inscripción',
+        descripcion: 'Esta el la decripcion de una recompensa de prueba',
+        puntosNecesarios: 100
+    },
+    {
+        id: 5,
+        titulo: 'Recompensa de prueba',
+        descripcion: 'Esta el la decripcion de una recompensa de prueba',
+        puntosNecesarios: 100
+    },
+    {
+        id: 6,
+        titulo: 'Recompensa de prueba',
+        descripcion: 'Esta el la decripcion de una recompensa de prueba',
+        puntosNecesarios: 100
+    }
+
+];
